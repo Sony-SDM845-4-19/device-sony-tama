56bae5b96e9b8880940c1726b601d5a00e419e66
Pavel Dubrova <pashadubrova@gmail.com>
2025-06-20 11:57:44 +0300

    etc: Provide audio_policy_volumes.xml
    The configuration is tuned specifically for the platform
    and is required instead of the generic version.
    
    Was taken from 52.1.A.0.532 stock firmware.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- platform.mk
+++ platform.mk
@@ -147,6 +147,7 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/sound_trigger_mixer_paths_wcd9340.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9340.xml \
     $(SONY_ROOT)/vendor/etc/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
     $(SONY_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(SONY_ROOT)/vendor/etc/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(SONY_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(SONY_ROOT)/vendor/etc/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml
 
